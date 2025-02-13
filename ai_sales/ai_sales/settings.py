00918be9328b1db025d6bcfec09d9f5101dee50d
Alex Mwakalikamo <59507616+Damace@users.noreply.github.com>
2025-02-14 02:03:06 +0300

    Your commit message3

--- ai_sales/ai_sales/settings.py
+++ ai_sales/ai_sales/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import dj_database_url
+from decouple import config 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,15 +24,12 @@ LANGUAGE_CODE = 'en-us'
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cwr(0&(my03w@-iio^hk=p3$5o7#k)q5fz@xx^eq63dj+u+mb$'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool)
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://9f58-41-59-180-180.ngrok-free.app"
-]
-ALLOWED_HOSTS = ["9f58-41-59-180-180.ngrok-free.app", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
 # Application definition
 
@@ -103,7 +102,7 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse("postgresql://stock_vision_user:ApFR5LCCZBldNFIU15ZS8cyl26UBvTwG@dpg-cun6pirqf0us73fak6s0-a.oregon-postgres.render.com/stock_vision")
+DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
 
 
 # Password validation
